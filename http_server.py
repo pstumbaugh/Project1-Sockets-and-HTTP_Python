c2da9f45263b76b19fb189bbd129b5ac0768f5d7
Pat <stumbaup@oregonstate.edu>
2021-04-18 13:21:23 -0700

    updated http_sever.py

--- http_server.py
+++ http_server.py
@@ -1,35 +1,51 @@
 # Patrick Stumbaugh
 
-# background info I found on end of message for sockets from:
-# https://stackoverflow.com/questions/41382127/how-does-the-python-socket-recv-method-know-that-the-end-of-the-message-has-be
+# code adapted from class textbook: chapter 2.7
 
 import socket  # for socket
 import sys  # for user to quit
 
+# what we'll be sending back to client
+data = "HTTP/1.1 200 OK\r\n"\
+    "Content-Type: text/html; charset=UTF-8\r\n\r\n"\
+    "<html>Congratulations! You've downloaded the first Wireshark lab file!</html>\r\n"
+
 # create socket and set server info
 serverName = 'http_server'
-serverPort = 80
+serverPort = 7777
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# connect and send GET message to server:
-clientSocket.connect((serverName, serverPort))
-sentence = 'GET /wireshark-labs/HTTP-wireshark-file3.html HTTP/1.1\r\nHost:gaia.cs.umass.edu\r\n\r\n'
-clientSocket.send(sentence.encode())  # send the message
+# bind port number to server socket
+serverSocket.bind(('', serverPort))
 
-fullMessage = ''  # initialise fulleMessage string
-
-while True:  # see if there are any bytes to print still
-    # receive the message
-    modifiedSentence = clientSocket.recv(1024)
-    # check if any bytes are left, if so, add to fullMessage and loop around, else break
-    if len(modifiedSentence) <= 0:
-        break
-    # add part of our message to the full message
-    fullMessage += modifiedSentence.decode('UTF-8')
-
-print('[RECV] - length: %s' % (len(fullMessage)))  # length of message
 print
-print(fullMessage)  # message
+print('Connected at : 127.0.0.1:7777')
+print
 
-# close the socket connection
-clientSocket.close()
+requestFlag = False  # flag to end request
+
+# wait for requests
+serverSocket.listen(1)
+print('The server is ready to receive')
+while True:
+    # accept a request
+    connectionSocket, addr = serverSocket.accept()
+    # receive the request
+    sentence = connectionSocket.recv(1024).decode()
+
+    requestFlag = True  # change flag to true
+
+    # print the request
+    print('RECEIVED: ')
+    print(sentence)
+    print
+    # print what we're sending to client
+    print('SENDING: ')
+    print(data)
+    print
+
+    # encode data and send back to client
+    connectionSocket.send(data.encode())
+    connectionSocket.close()  # closer socket
+    if requestFlag == True:
+        break
